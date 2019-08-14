aae3842ebe6c17b0418983b1153b0e5785160e2f
emilio barrera <barrera_emilio@hotmail.com>
2019-08-14 23:40:56 +0100

    Add project files.

---
+++ BlazorCRUD/Server/Controllers/PersonasController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorCRUD.Server.Data;
+using BlazorCRUD.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCRUD.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PersonasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Persona>>> GetPersonas() => await _context.Personas.ToListAsync();
+       
+    }
+}
---
+++ BlazorCRUD/Shared/Models/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorCRUD.Shared.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
