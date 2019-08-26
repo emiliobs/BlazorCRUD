77f352e6a2b85a77a3a324831535c995c5a9508c
emilio barrera <barrera_emilio@hotmail.com>
2019-08-27 00:20:44 +0100

    Confirm delete to regester with SweetAlert2(nice display)

--- BlazorCRUD/Client/Helpers/IJSExtensions.cs
+++ BlazorCRUD/Client/Helpers/IJSExtensions.cs
@@ -10,5 +10,25 @@ namespace BlazorCRUD.Client.Helpers
         {
           return js.InvokeAsync<object>("saveAsFile", nombreArchivo, Convert.ToBase64String(archivo));
         }
+
+        public static Task MostrarMensaje(this IJSRuntime js, string mensaje)
+        {
+            return js.InvokeAsync<object>("Swal.fire", mensaje);
+        }
+
+        public static Task MostrarMensaje(this IJSRuntime js, string titulo, string mensaje ,TipoMensajeSweetAlert tipo)
+        {
+            return js.InvokeAsync<object>("Swal.fire", titulo, mensaje, tipo.ToString());
+        }
+
+        public async static Task<bool> Confirm(this IJSRuntime js, string titulo, string mesaje, TipoMensajeSweetAlert tipo)
+        {
+            return await js.InvokeAsync<bool>("CustomConfirm", titulo , mesaje, tipo.ToString());
+        }
+
+        public enum TipoMensajeSweetAlert
+        {
+            question, warning, error, success, info
+        }
     }
 }
--- BlazorCRUD/Client/wwwroot/js/Utilidades.js
+++ BlazorCRUD/Client/wwwroot/js/Utilidades.js
@@ -8,3 +8,30 @@
     document.body.removeChild(link);
 
 }
+
+function CustomConfirm(titulo, mensaje, tipo) {
+
+
+    return new Promise((resolve) =>
+    {
+
+        Swal.fire({
+            title: titulo,
+            text: mensaje,
+            type: tipo,
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Confirmar'
+        }).then((result) => {
+            if (result.value) {
+                resolve(true);
+            }
+            else {
+                resolve(false);
+            }
+        });
+
+    });
+
+}
